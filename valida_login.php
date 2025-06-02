3286f347a8a003437dfe75f52d609c5e186f1ea5
Pedro Lucas <izielucas2016@gmail.com>
2025-06-02 13:54:01 -0300

    feat: added post method in validate login

---
+++ valida_login.php
@@ -0,0 +1,12 @@
+<?php
+    // //GET
+    // echo $_GET['email'];
+    // echo '<br>';
+    // echo $_GET['senha'];
+
+    print_r($_POST);
+    echo '<br>';
+    echo $_POST['email'];
+    echo '<br>';
+    echo $_POST['senha'];
+?>
