300ef4460e38e9b06a235f80ea9b2786a35e4d83
Pedro Lucas <izielucas2016@gmail.com>
2025-06-03 13:14:46 -0300

    feat: added data validation

--- valida_login.php
+++ valida_login.php
@@ -1,12 +1,35 @@
 <?php
+
+    $usuario_autenticado = false;
+    $usuarios_app = [
+        ['email'=>'admin@test.com', 'senha'=>'1234'],
+        ['email'=>'user@test.com', 'senha'=>'4321']
+    ];
+
+    // echo '<pre>';
+    // print_r($usuarios_app);
+    // echo '</pre>';
+
+    foreach ($usuarios_app as $user){
+        if(!$usuario_autenticado && ($_POST['email'] == $user['email'] && $_POST['senha'] == $user['senha'])){
+            $usuario_autenticado =  true;
+        }
+    }
+
+    if($usuario_autenticado){
+        echo 'Usuario Autenticado!';
+    }else{
+        header('Location: index.php?login=erro');
+    }
+
     // //GET
     // echo $_GET['email'];
     // echo '<br>';
     // echo $_GET['senha'];
 
-    print_r($_POST);
-    echo '<br>';
-    echo $_POST['email'];
-    echo '<br>';
-    echo $_POST['senha'];
+    // print_r($_POST);
+    // echo '<br>';
+    // echo $_POST['email'];
+    // echo '<br>';
+    // echo $_POST['senha'];
 ?>
