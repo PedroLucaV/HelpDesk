314418ec0d1fce030bc1ac5a5e0fd5d13a09ba27
Pedro Lucas <izielucas2016@gmail.com>
2025-06-05 13:39:44 -0300

    Improves call display and registration
    Ensures only relevant calls are displayed to users
    based on their ID and role, and includes the user ID
    when registering a new call.

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,10 +1,12 @@
 <?php
+
+session_start();
 $titulo = str_replace('#', '-', $_POST['titulo']);
 $categoria = str_replace('#', '-', $_POST['categoria']);
 $descricao = str_replace('#', '-', $_POST['descricao']);
 
 
-$text = "{$titulo}#{$categoria}#{$descricao}" . PHP_EOL;
+$text = "{$_SESSION['id']}#{$titulo}#{$categoria}#{$descricao}" . PHP_EOL;
 
 $arquivo = fopen('arquivo.txt', 'a');
 fwrite($arquivo, $text);
--- valida_login.php
+++ valida_login.php
@@ -6,10 +6,16 @@
     // print_r($_SESSION);
 
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
     $usuarios_app = [
-        ['email'=>'admin@test.com', 'senha'=>'1234'],
-        ['email'=>'user@test.com', 'senha'=>'4321']
+        ['id'=>1,'email'=>'admin@test.com', 'senha'=>'1234', 'perfil_id'=>1],
+        ['id'=>2,'email'=>'user@test.com', 'senha'=>'1234', 'perfil_id'=>2],
+        ['id'=>3,'email' => 'jose@test.com', 'senha' => '1234', 'perfil_id'=>2],
+        ['id'=>4,'email' => 'maria@test.com', 'senha' => '1234', 'perfil_id'=>2]
     ];
+
+    $perfis=[1=>'Administrativo', 2=>'Usuário'];
 
     // echo '<pre>';
     // print_r($usuarios_app);
@@ -18,12 +24,16 @@
     foreach ($usuarios_app as $user){
         if(!$usuario_autenticado && ($_POST['email'] == $user['email'] && $_POST['senha'] == $user['senha'])){
             $usuario_autenticado =  true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
         }
     }
     // echo '<hr>';
     if($usuario_autenticado){
         // echo 'Usuario Autenticado!';
         $_SESSION['AUTH'] = true;
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
         header('Location: home.php');
     }else{
         $_SESSION['AUTH'] = false;
